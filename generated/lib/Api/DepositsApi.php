c666c8bb4742e42dcf523212678dcf45a22401c7
AurePay SDK Sync <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-27 16:18:53 +0000

    chore(sdk): sync from aurepay monorepo (2026-07-27T16:18:53.391Z)

--- generated/lib/Api/DepositsApi.php
+++ generated/lib/Api/DepositsApi.php
@@ -80,6 +80,9 @@ class DepositsApi
         'depositsGet' => [
             'application/json',
         ],
+        'depositsGetPayload' => [
+            'application/json',
+        ],
         'depositsList' => [
             'application/json',
         ],
@@ -650,6 +653,298 @@ class DepositsApi
 
 
         $resourcePath = '/deposits/{id}';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+
+
+        // path params
+        if ($id !== null) {
+            $resourcePath = str_replace(
+                '{id}',
+                ObjectSerializer::toPathValue($id),
+                $resourcePath
+            );
+        }
+
+
+        $headers = $this->headerSelector->selectHeaders(
+            ['application/json', ],
+            $contentType,
+            $multipart
+        );
+
+        // for model (json/xml)
+        if (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $formParamValueItems = is_array($formParamValue) ? $formParamValue : [$formParamValue];
+                    foreach ($formParamValueItems as $formParamValueItem) {
+                        $multipartContents[] = [
+                            'name' => $formParamName,
+                            'contents' => $formParamValueItem
+                        ];
+                    }
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+
+            } elseif (stripos($headers['Content-Type'], 'application/json') !== false) {
+                # if Content-Type contains "application/json", json_encode the form parameters
+                $httpBody = \GuzzleHttp\Utils::jsonEncode($formParams);
+            } else {
+                // for HTTP post (form)
+                $httpBody = ObjectSerializer::buildQuery($formParams);
+            }
+        }
+
+        // this endpoint requires API key authentication
+        $apiKey = $this->config->getApiKeyWithPrefix('X-Api-Key');
+        if ($apiKey !== null) {
+            $headers['X-Api-Key'] = $apiKey;
+        }
+        // this endpoint requires API key authentication
+        $apiKey = $this->config->getApiKeyWithPrefix('X-Api-Secret');
+        if ($apiKey !== null) {
+            $headers['X-Api-Secret'] = $apiKey;
+        }
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $operationHost = $this->config->getHost();
+        $query = ObjectSerializer::buildQuery($queryParams);
+        return new Request(
+            'GET',
+            $operationHost . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
+
+    /**
+     * Operation depositsGetPayload
+     *
+     * Payload request/response do depósito
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['depositsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \AurePayGenerated\ApiException on non-2xx response or if the response body is not in the expected format
+     * @throws \InvalidArgumentException
+     * @return \AurePayGenerated\Model\SuccessEnvelope|\AurePayGenerated\Model\ErrorBody
+     */
+    public function depositsGetPayload($id, string $contentType = self::contentTypes['depositsGetPayload'][0])
+    {
+        list($response) = $this->depositsGetPayloadWithHttpInfo($id, $contentType);
+        return $response;
+    }
+
+    /**
+     * Operation depositsGetPayloadWithHttpInfo
+     *
+     * Payload request/response do depósito
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['depositsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \AurePayGenerated\ApiException on non-2xx response or if the response body is not in the expected format
+     * @throws \InvalidArgumentException
+     * @return array of \AurePayGenerated\Model\SuccessEnvelope|\AurePayGenerated\Model\ErrorBody, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function depositsGetPayloadWithHttpInfo($id, string $contentType = self::contentTypes['depositsGetPayload'][0])
+    {
+        $request = $this->depositsGetPayloadRequest($id, $contentType);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    (int) $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? (string) $e->getResponse()->getBody() : null
+                );
+            } catch (ConnectException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    (int) $e->getCode(),
+                    null,
+                    null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+
+            switch($statusCode) {
+                case 200:
+                    return $this->handleResponseWithDataType(
+                        '\AurePayGenerated\Model\SuccessEnvelope',
+                        $request,
+                        $response,
+                    );
+                case 404:
+                    return $this->handleResponseWithDataType(
+                        '\AurePayGenerated\Model\ErrorBody',
+                        $request,
+                        $response,
+                    );
+            }
+
+            
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        (string) $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    (string) $response->getBody()
+                );
+            }
+
+            return $this->handleResponseWithDataType(
+                '\AurePayGenerated\Model\SuccessEnvelope',
+                $request,
+                $response,
+            );
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\AurePayGenerated\Model\SuccessEnvelope',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    throw $e;
+                case 404:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\AurePayGenerated\Model\ErrorBody',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    throw $e;
+            }
+        
+
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation depositsGetPayloadAsync
+     *
+     * Payload request/response do depósito
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['depositsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function depositsGetPayloadAsync($id, string $contentType = self::contentTypes['depositsGetPayload'][0])
+    {
+        return $this->depositsGetPayloadAsyncWithHttpInfo($id, $contentType)
+            ->then(
+                function ($response) {
+                    return $response[0];
+                }
+            );
+    }
+
+    /**
+     * Operation depositsGetPayloadAsyncWithHttpInfo
+     *
+     * Payload request/response do depósito
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['depositsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function depositsGetPayloadAsyncWithHttpInfo($id, string $contentType = self::contentTypes['depositsGetPayload'][0])
+    {
+        $returnType = '\AurePayGenerated\Model\SuccessEnvelope';
+        $request = $this->depositsGetPayloadRequest($id, $contentType);
+
+        return $this->client
+            ->sendAsync($request, $this->createHttpClientOption())
+            ->then(
+                function ($response) use ($returnType) {
+                    if ($returnType === '\SplFileObject') {
+                        $content = $response->getBody(); //stream goes to serializer
+                    } else {
+                        $content = (string) $response->getBody();
+                        if ($returnType !== 'string') {
+                            $content = json_decode($content);
+                        }
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+                },
+                function ($exception) {
+                    $response = $exception->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    throw new ApiException(
+                        sprintf(
+                            '[%d] Error connecting to the API (%s)',
+                            $statusCode,
+                            $exception->getRequest()->getUri()
+                        ),
+                        $statusCode,
+                        $response->getHeaders(),
+                        (string) $response->getBody()
+                    );
+                }
+            );
+    }
+
+    /**
+     * Create request for operation 'depositsGetPayload'
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['depositsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    public function depositsGetPayloadRequest($id, string $contentType = self::contentTypes['depositsGetPayload'][0])
+    {
+
+        // verify the required parameter 'id' is set
+        if ($id === null || (is_array($id) && count($id) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $id when calling depositsGetPayload'
+            );
+        }
+
+
+        $resourcePath = '/deposits/{id}/payload';
         $formParams = [];
         $queryParams = [];
         $headerParams = [];
--- generated/lib/Api/WithdrawalsApi.php
+++ generated/lib/Api/WithdrawalsApi.php
@@ -80,6 +80,9 @@ class WithdrawalsApi
         'withdrawalsGet' => [
             'application/json',
         ],
+        'withdrawalsGetPayload' => [
+            'application/json',
+        ],
         'withdrawalsList' => [
             'application/json',
         ],
@@ -605,6 +608,298 @@ class WithdrawalsApi
 
 
         $resourcePath = '/withdrawals/{id}';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+
+
+        // path params
+        if ($id !== null) {
+            $resourcePath = str_replace(
+                '{id}',
+                ObjectSerializer::toPathValue($id),
+                $resourcePath
+            );
+        }
+
+
+        $headers = $this->headerSelector->selectHeaders(
+            ['application/json', ],
+            $contentType,
+            $multipart
+        );
+
+        // for model (json/xml)
+        if (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $formParamValueItems = is_array($formParamValue) ? $formParamValue : [$formParamValue];
+                    foreach ($formParamValueItems as $formParamValueItem) {
+                        $multipartContents[] = [
+                            'name' => $formParamName,
+                            'contents' => $formParamValueItem
+                        ];
+                    }
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+
+            } elseif (stripos($headers['Content-Type'], 'application/json') !== false) {
+                # if Content-Type contains "application/json", json_encode the form parameters
+                $httpBody = \GuzzleHttp\Utils::jsonEncode($formParams);
+            } else {
+                // for HTTP post (form)
+                $httpBody = ObjectSerializer::buildQuery($formParams);
+            }
+        }
+
+        // this endpoint requires API key authentication
+        $apiKey = $this->config->getApiKeyWithPrefix('X-Api-Key');
+        if ($apiKey !== null) {
+            $headers['X-Api-Key'] = $apiKey;
+        }
+        // this endpoint requires API key authentication
+        $apiKey = $this->config->getApiKeyWithPrefix('X-Api-Secret');
+        if ($apiKey !== null) {
+            $headers['X-Api-Secret'] = $apiKey;
+        }
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $operationHost = $this->config->getHost();
+        $query = ObjectSerializer::buildQuery($queryParams);
+        return new Request(
+            'GET',
+            $operationHost . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
+
+    /**
+     * Operation withdrawalsGetPayload
+     *
+     * Payload request/response do saque
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['withdrawalsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \AurePayGenerated\ApiException on non-2xx response or if the response body is not in the expected format
+     * @throws \InvalidArgumentException
+     * @return \AurePayGenerated\Model\SuccessEnvelope|\AurePayGenerated\Model\ErrorBody
+     */
+    public function withdrawalsGetPayload($id, string $contentType = self::contentTypes['withdrawalsGetPayload'][0])
+    {
+        list($response) = $this->withdrawalsGetPayloadWithHttpInfo($id, $contentType);
+        return $response;
+    }
+
+    /**
+     * Operation withdrawalsGetPayloadWithHttpInfo
+     *
+     * Payload request/response do saque
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['withdrawalsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \AurePayGenerated\ApiException on non-2xx response or if the response body is not in the expected format
+     * @throws \InvalidArgumentException
+     * @return array of \AurePayGenerated\Model\SuccessEnvelope|\AurePayGenerated\Model\ErrorBody, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function withdrawalsGetPayloadWithHttpInfo($id, string $contentType = self::contentTypes['withdrawalsGetPayload'][0])
+    {
+        $request = $this->withdrawalsGetPayloadRequest($id, $contentType);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    (int) $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? (string) $e->getResponse()->getBody() : null
+                );
+            } catch (ConnectException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    (int) $e->getCode(),
+                    null,
+                    null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+
+            switch($statusCode) {
+                case 200:
+                    return $this->handleResponseWithDataType(
+                        '\AurePayGenerated\Model\SuccessEnvelope',
+                        $request,
+                        $response,
+                    );
+                case 404:
+                    return $this->handleResponseWithDataType(
+                        '\AurePayGenerated\Model\ErrorBody',
+                        $request,
+                        $response,
+                    );
+            }
+
+            
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        (string) $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    (string) $response->getBody()
+                );
+            }
+
+            return $this->handleResponseWithDataType(
+                '\AurePayGenerated\Model\SuccessEnvelope',
+                $request,
+                $response,
+            );
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\AurePayGenerated\Model\SuccessEnvelope',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    throw $e;
+                case 404:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\AurePayGenerated\Model\ErrorBody',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    throw $e;
+            }
+        
+
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation withdrawalsGetPayloadAsync
+     *
+     * Payload request/response do saque
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['withdrawalsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function withdrawalsGetPayloadAsync($id, string $contentType = self::contentTypes['withdrawalsGetPayload'][0])
+    {
+        return $this->withdrawalsGetPayloadAsyncWithHttpInfo($id, $contentType)
+            ->then(
+                function ($response) {
+                    return $response[0];
+                }
+            );
+    }
+
+    /**
+     * Operation withdrawalsGetPayloadAsyncWithHttpInfo
+     *
+     * Payload request/response do saque
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['withdrawalsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function withdrawalsGetPayloadAsyncWithHttpInfo($id, string $contentType = self::contentTypes['withdrawalsGetPayload'][0])
+    {
+        $returnType = '\AurePayGenerated\Model\SuccessEnvelope';
+        $request = $this->withdrawalsGetPayloadRequest($id, $contentType);
+
+        return $this->client
+            ->sendAsync($request, $this->createHttpClientOption())
+            ->then(
+                function ($response) use ($returnType) {
+                    if ($returnType === '\SplFileObject') {
+                        $content = $response->getBody(); //stream goes to serializer
+                    } else {
+                        $content = (string) $response->getBody();
+                        if ($returnType !== 'string') {
+                            $content = json_decode($content);
+                        }
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+                },
+                function ($exception) {
+                    $response = $exception->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    throw new ApiException(
+                        sprintf(
+                            '[%d] Error connecting to the API (%s)',
+                            $statusCode,
+                            $exception->getRequest()->getUri()
+                        ),
+                        $statusCode,
+                        $response->getHeaders(),
+                        (string) $response->getBody()
+                    );
+                }
+            );
+    }
+
+    /**
+     * Create request for operation 'withdrawalsGetPayload'
+     *
+     * @param  string $id Identificador ULID do recurso (required)
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['withdrawalsGetPayload'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    public function withdrawalsGetPayloadRequest($id, string $contentType = self::contentTypes['withdrawalsGetPayload'][0])
+    {
+
+        // verify the required parameter 'id' is set
+        if ($id === null || (is_array($id) && count($id) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $id when calling withdrawalsGetPayload'
+            );
+        }
+
+
+        $resourcePath = '/withdrawals/{id}/payload';
         $formParams = [];
         $queryParams = [];
         $headerParams = [];
